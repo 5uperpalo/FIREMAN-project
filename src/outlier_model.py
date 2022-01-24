22d94efce4d112b14bf6b0e285c1fd440689a547
Alexander Beattie <alexander.beattie@quarkworks.co>
2022-01-24 04:24:09 -0600

    Remove dead code Integrate with deep learning system

--- src/outlier_model.py
+++ src/outlier_model.py
@@ -2,9 +2,6 @@ import logging
 
 import numpy as np
 import stumpy
-from pysad.transform.probability_calibration import ConformalProbabilityCalibrator
-
-from src import peak_detection
 
 logging.basicConfig(format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logger = logging.getLogger("TimeSeries")
@@ -19,34 +16,21 @@ class OutlierModel:
             m: int = 15,
             std_dev: int = 3,
             time_series=None,
-            egress=True,
-            lag: float = 100,
-            threshold: float = 5,
-            influence: float = 0.5
+            egress=True
     ):
         self.m = m
         if time_series is None:
             time_series = np.zeros(self.m * 4)
         self.time_series = time_series
+        self.ts_size = len(self.time_series)
         self.stream = stumpy.stumpi(self.time_series, m, egress=egress, normalize=False)
-        self.lastMaxIndex = -1
         self.anomalies = []
         self.std_dev = std_dev
         self.count = 0
         self.max_std_dev = []
         self.max_mean = []
         self.max_val = []
-        self.comparisson = []
-        self.avgFilter = []
         self.stdFilter = []
-        # Settings: lag = 30, threshold = 5, influence = 0
-        self.lag = lag
-        self.threshold = threshold
-        self.influence = influence
-        self.peak_detection = peak_detection.real_time_peak_detection(np.zeros(self.lag), self.lag, self.threshold,
-                                                                      self.influence)
-        logger.warning(f"Lag: {self.lag} Threshold: {self.threshold} Influence: {self.influence}")
-        # self.calibrator = ConformalProbabilityCalibrator(windowed=True, window_size=len(self.time_series) * 5)
 
     def __repr__(self):
         return str(self.__dict__)
@@ -66,46 +50,24 @@ class OutlierModel:
         self.max_mean.append(mean_mp)
         self.max_std_dev.append(std_dev_mp)
 
-        # max_index = np.argwhere(self.stream.P_ == self.stream.P_.max()).flatten()[0]
-        anomaly = False
         true_anomaly = False
-        metric = self.peak_detection.thresholding_algo(max_mp)
-        self.stdFilter.append(metric['stdFilter'])
-        self.avgFilter.append(metric['avgFilter'])
-        # if self.count >= self.lag:
-        #     local_window = self.max_val[-self.lag:]
-        #     mean_local = np.average(local_window)
-        #     std_dev_local = np.std(local_window)
-        #     metric = (max_mp - mean_local) / std_dev_local
-        # else:
-        #     metric = 0
-        # self.peak_detection = add(self.peak_detection, max_index, self.lag, self.threshold, self.peak_detection)
 
-        self.comparisson.append(metric['signal'])
+        metric = (mean_mp + (std_dev_mp * self.std_dev))
+        self.stdFilter.append(metric)
 
-        # metric =(mean_mp + (std_dev_mp * self.std_dev))
-        # self.stdFilter.append(metric)
-
-        if metric['signal'] == 1:
-            # if max_mp > metric:
-            anomaly = True
-            # if self.lastMaxIndex >= 0:
-            #     if self.lastMaxIndex != max_index:
-            #         anomaly = True
-            # else:
-            #     anomaly = True
-        if anomaly and self.filter(index, max_mp):
+        if (max_mp > metric) and (abs(metric - max_mp) > 0.01) and self.filter(index):
             true_anomaly = True
             self.anomalies.append(index)
-            logger.warning(f" Anomaly at Global index: {index}")
-        # self.lastMaxIndex = max_index
+            logger.info(f" Anomaly at Global index: {index}")
+            logger.info(f"max_mp: {max_mp}, metric:{metric}: metric-max_mp: {abs(metric - max_mp)}")
         return true_anomaly
 
-    def filter(self, index, max_mp):
-        return (not self.is_warming_up()) and (not self.recent_fault(index)) and max_mp > 1
+    def filter(self, index):
+        return (not self.is_warming_up()) and (not self.recent_fault(index))
 
     def is_warming_up(self):
         return self.count <= self.m
 
     def recent_fault(self, index):
-        return index < (self.anomalies[-1] + 10000) if self.anomalies else False # TODO: parmeterize window size
+        return index < (
+                    self.anomalies[-1] + self.ts_size * 2) if self.anomalies else False  # TODO: parmeterize window size
